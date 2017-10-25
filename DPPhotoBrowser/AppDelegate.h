d8d16d47a20b783c32eb82683d72fc92920d68b1
Roc <408937078@qq.com>
2017-10-25 09:17:33 +0800

    iOS相册图片选择

---
+++ DPPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DPPhotoBrowser
+//
+//  Created by roc on 2017/10/25.
+//  Copyright © 2017年 roc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
